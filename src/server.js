acf0ff5f5e9ed0eaa63bdbec8130c142b91a497d
nmonsma <78513959+nmonsma@users.noreply.github.com>
2021-05-17 10:27:09 -0700

    bugfix: removed language leftover from template

--- src/server.js
+++ src/server.js
@@ -37,7 +37,7 @@ app.use(express.static('client'))
 
 // Listen on port 3000
 app.listen(3000, function () {
-    console.log('Example app listening on port 3000!')
+    console.log('Listening on port 3000')
 })
 
 //Get route for the data
