29092b4b6adeadc4850b16497b7932eba26d32f4
Nick Monsma <nmonsma@gmail.com>
2021-05-20 13:23:47 -0700

    bugfix: changed the fs functions to promises
    In an attempt to figure out what was going wrong on the plex server,
    changed fs.writeFile and fs.readFile to promises.

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 //Configure the app to use Express:
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-const fs = require('fs');
+const { writeFile, readFile } = require('fs').promises;
 
 /*Spin up the Server*/
 app.use(express.static('client'))
@@ -18,44 +18,41 @@ app.listen(3000, function () {
 })
 
 //Get route for the data
-app.get('/retrieve', sendData);
-    function sendData (request, response) {
-        fs.readFile ('schedule.json', 'utf8', (err, scheduleData)=> {
-            if (err) throw err;
-            response.send(scheduleData);
-        })
+app.get('/retrieve', async (request, response)=> {
+    try {
+        const file = await readFile('schedule.json');
+        console.log('sending file contents');
+        response.send(file);
+    } catch (error) {
+        console.log(error);
     }
+})
 
 //Post route for adding a name and returning the full data
-app.post('/add', addData);
-    function addData (request, response) {
+app.post('/add', async (request, response)=>{
+    try {
+        const file = await readFile('schedule.json');
+        let scheduleData = JSON.parse(file);
 
-        fs.readFile ('schedule.json', 'utf8', (err, file)=> {
-            if (err) throw err;
-            let scheduleData = JSON.parse(file);
+        let dateIndex = 0;
+        
+        for (i=0; i<scheduleData.data.length; i++) { //look for the 'date' as the 'date' data in the indexed array item.
+            if (scheduleData.data[i].date == request.body.date) dateIndex = i;
+        }
 
-            let dateIndex = 0;
-            
-            for (i=0; i<scheduleData.data.length; i++) { //look for the 'date' as the 'date' data in the indexed array item.
-                if (scheduleData.data[i].date == request.body.date) dateIndex = i;
+        for (i=0; i<scheduleData.data[dateIndex].prayerSlots.length; i++) {    //find the prayer slot        
+            if (scheduleData.data[dateIndex].prayerSlots[i].index == request.body.index) {
+                const prayerName = ` ${request.body.firstName} ${request.body.lastInitial}`;
+                scheduleData.data[dateIndex].prayerSlots[i].names.push(prayerName);
             }
-
-            for (i=0; i<scheduleData.data[dateIndex].prayerSlots.length; i++) {    //find the prayer slot        
-                if (scheduleData.data[dateIndex].prayerSlots[i].index == request.body.index) {
-                    const prayerName = ` ${request.body.firstName} ${request.body.lastInitial}`;
-                    scheduleData.data[dateIndex].prayerSlots[i].names.push(prayerName);
-                }
-            }
-            
-            response.send(scheduleData);
-            
-            const jsonContent = JSON.stringify(scheduleData);
-            fs.writeFile('schedule.json', jsonContent, 'utf8', (err)=> {
-                if (err) {
-                    console.log ("Error writing to file:", err);
-                } else {
-                    console.log("File saved.");
-                }
-            })
-        })
+        }
+        
+        response.send(scheduleData);
+        
+        const jsonContent = JSON.stringify(scheduleData);
+        
+        const write = await writeFile('schedule.json', jsonContent);
+    } catch (error) {
+        console.log(error);
     }
+})
